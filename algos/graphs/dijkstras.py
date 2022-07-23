f1b1abb3f489700b81d736e2386edea1e85c20d0
kevin key <kevdog8642@gmail.com>
2022-07-23 13:19:16 -0500

    add comments to dijkstras and add to/edit readme

--- algos/graphs/dijkstras.py
+++ algos/graphs/dijkstras.py
@@ -10,7 +10,19 @@ class Graph:
         self.nodes = nodes
         self.size = len(nodes)
 
+# use case - single-pair shortest path (SPSP). ONLY works
+#            if you're guaranteed not to have negative
+#            edges
+# high level - from predefined starting node, add neighbors
+#              with running total weight to heap. as you 
+#              process, don't forget to check and see if
+#              what you already have in the dist[] is less
+#              than what was just popped off the heap
+
 def reconstruct_paths(ancestor_arr, node_idx_map: dict):
+    # idx_node_map is used for bijection - bidirectional
+    # dictionary. probly a better way to do it, but this
+    # approach is good enough for this use case
     idx_node_map = {}
     for node_val,idx in node_idx_map.items():
         idx_node_map[idx] = node_val
@@ -21,21 +33,27 @@ def reconstruct_paths(ancestor_arr, node_idx_map: dict):
         res = []
         curr_idx = idx
         curr_val = node_val
+        # gogogo til you reach a node whose ancestor is
+        # None (found the start node)
         while ancestor_arr[curr_idx] is not None:
             res.append(curr_val)
             curr_idx = ancestor_arr[curr_idx]
             curr_val = idx_node_map[curr_idx]
         res.append(curr_val)
+        # iterating in reverse, for non-pythoners
         for i in range(len(res)-1,-1,-1):
             print(res[i],"=>",end=" ")
         print("\n")
 
 def dijkstras(graph: Graph):
+    # node_idx_map is used for access to min_dist[] and ancestor[]
+    # since our vertices' values are letters, not nums
     min_dist,node_idx_map,ancestor_arr = [],{},[]
     for i in range(len(graph.nodes)):
         min_dist.append(float("inf"))
         node_idx_map[graph.nodes[i].val] = i
         ancestor_arr.append(None)
+    # picking node here for simplicity rather than passing in
     start_node = graph.nodes[0]
     start_node_idx = node_idx_map[start_node.val]
     min_dist[start_node_idx] = 0
@@ -46,6 +64,8 @@ def dijkstras(graph: Graph):
     while heap:
         curr_wt,ancestor_idx,curr_end = heapq.heappop(heap)
         curr_end_idx = node_idx_map[curr_end.val]
+        # skip if what's in min_dist is already <= what's being
+        # proposed
         if min_dist[curr_end_idx] <= curr_wt:
             continue
         min_dist[curr_end_idx] = curr_wt
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 trying to code up the common data structures/algos all in one place. hoping this may help at least one person put the pieces together when it comes to data structures and algos. i'm not a python whiz, so the code should be relatively straightforward to read and comprehend (hopefully). trying to leave comments as i go - enough to get the point across but not so much as to be considered a fkn book
 
+if you're like me and some things just don't click for whatever reason, pull down the repo and debug to see what's happening under the hood. besides drawing sht out, this is my favorite way to attempt to understand what's goin on. all .py files should be debuggable as is
+
 i don't have a phd/masters/undergrad degree in comp sci, my iq isn't 180, and i'm very bad at s3x. if you see room for improvement or spot a bug, don't be a dick about it - my self-esteem can't take too many more hits. just lmk or raise a pr (idk what the non-enterprise/open-source standards are)
 
-glhf
+ps, imo, i think it's a best practice to actually code this sht out yourself. maybe try it in your preferred language, and raise a pr here if you want to contribute so that, in the unlikely event this gets lotsa viewers, there will be solutions in various langs
+
+thanks, glhf
