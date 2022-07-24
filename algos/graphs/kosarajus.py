ef78e7af744500ae18f220bd13e22ef1992d8adf
kevin key <kevdog8642@gmail.com>
2022-07-24 10:58:29 -0500

    add kosaraju's algo for SCCs, thanks to trincot from SO for contribution to graph inversion

--- algos/graphs/kosarajus.py
+++ algos/graphs/kosarajus.py
@@ -25,6 +25,41 @@ class Graph:
                     break
                 neighbor.neighbors.append((node, weight))
     
+# use case - strongly connected components
+# high level - look at it like you're doing a topological sort,
+#              inverting the graph, then doing another top sort
+
+def kosarajus(graph: Graph):
+    # need val_node_map for our second traversal
+    visited,stack,val_node_map = set(),[],{}
+    def dfs(node: GraphNode):
+        val_node_map[node.val] = node
+        visited.add(node.val)
+        for neigh,_ in node.neighbors:
+            neigh.val not in visited and dfs(neigh)
+        stack.append(node.val)
+    for node in graph.nodes:
+        node.val not in visited and dfs(node)
+    # housekeeping
+    visited.clear()
+    # create copy of stack to pop from since defined dfs() uses
+    # the stack array by default
+    stack_copy = stack[:]
+    graph.invert_graph()
+    res = []
+    while stack_copy:
+        curr_node_val = stack_copy.pop()
+        curr_node = val_node_map[curr_node_val]
+        if curr_node_val not in visited:
+            # in dfs(), we're already appending to stack. thus,
+            # we can take advantage by clearing out the stack
+            # before each traversal and append the resultant
+            # stack to our final res[]
+            stack = []
+            dfs(curr_node)
+            res.append(stack)
+    return res
+
 if __name__ == "__main__":
     zero = GraphNode(0)
     one = GraphNode(1)
@@ -41,5 +76,6 @@ if __name__ == "__main__":
     six.neighbors = [(five, 2)]
     arr = [zero,one,two,three,four,five,six]
     g = Graph(arr)
-    g.invert_graph()
+    res = kosarajus(g)
+    print(res)
     
